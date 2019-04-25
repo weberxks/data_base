46f32a3d26a6593fec740d60c2cd619211905774
weberxks <49833712+weberxks@users.noreply.github.com>
2019-04-25 12:54:52 +0300

    Delete lesson_1.sql

--- lesson_1.sql
+++
@@ -1,9 +0,0 @@
-CREATE database Example
-show databases;
-use example;
-create table users (id INT(20), name text(20));
-describe users;
-exit;
-mysqldump -u root -p example > /usr/dump/sample.SQL
-mysql
-sample <sample.sql 
